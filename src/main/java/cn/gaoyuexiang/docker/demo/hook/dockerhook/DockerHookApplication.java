97b77f525ea3974c850978eee5c79e5d4757d48d
Yuexiang Gao <yxgao@thoughtworks.com>
2017-11-22 08:50:32 +0800

    init project

---
+++ src/main/java/cn/gaoyuexiang/docker/demo/hook/dockerhook/DockerHookApplication.java
@@ -0,0 +1,12 @@
+package cn.gaoyuexiang.docker.demo.hook.dockerhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerHookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerHookApplication.class, args);
+	}
+}

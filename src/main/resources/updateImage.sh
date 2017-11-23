f409788befe0366eea210673571e7e24e139059c
Yuexiang Gao <yxgao@thoughtworks.com>
2017-11-23 20:26:08 +0800

    update shell

--- src/main/resources/updateImage.sh
+++ src/main/resources/updateImage.sh
@@ -1,6 +1,7 @@
 container=spring_boot_demo
 
 docker stop ${container}
+docker rm ${container}
 docker rmi yuexiang/spring-boot-demo
 docker pull yuexiang/spring-boot-demo
 docker run -d --name=${container} --network=host yuexiang/spring-boot-demo

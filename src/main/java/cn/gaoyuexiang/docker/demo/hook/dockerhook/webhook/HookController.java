201b3b4eabee5abbb022606d62f323e82afc11e7
Yuexiang Gao <yxgao@thoughtworks.com>
2017-11-23 20:40:29 +0800

    update exec string

--- src/main/java/cn/gaoyuexiang/docker/demo/hook/dockerhook/webhook/HookController.java
+++ src/main/java/cn/gaoyuexiang/docker/demo/hook/dockerhook/webhook/HookController.java
@@ -1,6 +1,7 @@
 package cn.gaoyuexiang.docker.demo.hook.dockerhook.webhook;
 
 import java.io.IOException;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ public class HookController {
 
     @PostMapping("/webhook/spring-boot-demo")
     public void updateImage() throws IOException {
-        Runtime.getRuntime().exec("updateImage.sh");
+        Runtime.getRuntime().exec("/home/yxgao/updateImage.sh");
     }
     
 }

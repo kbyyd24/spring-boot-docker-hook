2e4ec7d274aa2bb1eb27de4b532bec046cd74a5b
Yuexiang Gao <yxgao@thoughtworks.com>
2017-11-23 21:16:24 +0800

    run without shell file

--- src/main/java/cn/gaoyuexiang/docker/demo/hook/dockerhook/webhook/HookController.java
+++ src/main/java/cn/gaoyuexiang/docker/demo/hook/dockerhook/webhook/HookController.java
@@ -5,12 +5,21 @@ import java.io.IOException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static java.lang.String.format;
+
 @RestController
 public class HookController {
 
     @PostMapping("/webhook/spring-boot-demo")
     public void updateImage() throws IOException {
-        Runtime.getRuntime().exec("updateImage.sh");
+        String container = "spring_boot_demo";
+        String image = "yuexiang/spring-boot-demo";
+
+        Runtime.getRuntime().exec(format("docker stop %s", container));
+        Runtime.getRuntime().exec(format("docker rm %s", container));
+        Runtime.getRuntime().exec(format("docker rmi %s", image));
+        Runtime.getRuntime().exec(format("docker pull %s", image));
+        Runtime.getRuntime().exec(format("docker run -d --name=%s --network=host %s", container, image));
     }
 
 }

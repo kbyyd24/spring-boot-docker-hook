2a85924891272300137f695f6bc0f7b6df8e8ff2
Yuexiang Gao <yxgao@thoughtworks.com>
2017-11-24 08:21:06 +0800

    print result

--- src/main/java/cn/gaoyuexiang/docker/demo/hook/dockerhook/webhook/HookController.java
+++ src/main/java/cn/gaoyuexiang/docker/demo/hook/dockerhook/webhook/HookController.java
@@ -1,6 +1,8 @@
 package cn.gaoyuexiang.docker.demo.hook.dockerhook.webhook;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,14 @@ public class HookController {
         String container = "spring_boot_demo";
         String image = "yuexiang/spring-boot-demo";
 
-        Runtime.getRuntime().exec(format(
+        Process proc = Runtime.getRuntime().exec(format(
                 "docker stop %s && docker rm %s && docker rmi %s && docker pull %s && docker run -d --name=%s --network=host %s",
                 container, container, image, image, container, image));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        String s = null;
+        while((s = reader.readLine())!=null) {
+            System.out.println(s);
+        }
     }
 
 }

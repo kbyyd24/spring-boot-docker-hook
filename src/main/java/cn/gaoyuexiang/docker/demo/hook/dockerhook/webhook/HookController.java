b1b8c23792dc9308becb6decd26bfc79cf000c27
Yuexiang Gao <yxgao@thoughtworks.com>
2017-11-24 08:43:37 +0800

    remove non-using import

--- src/main/java/cn/gaoyuexiang/docker/demo/hook/dockerhook/webhook/HookController.java
+++ src/main/java/cn/gaoyuexiang/docker/demo/hook/dockerhook/webhook/HookController.java
@@ -1,13 +1,9 @@
 package cn.gaoyuexiang.docker.demo.hook.dockerhook.webhook;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static java.lang.String.format;
 
 @RestController
 public class HookController {

897c45ca16b1bd3de29eeaa1acd9d47cc04ccb92
Yuexiang Gao <yxgao@thoughtworks.com>
2017-11-25 18:09:55 +0800

    add logger

--- src/main/java/cn/gaoyuexiang/docker/demo/hook/dockerhook/webhook/HookController.java
+++ src/main/java/cn/gaoyuexiang/docker/demo/hook/dockerhook/webhook/HookController.java
@@ -2,15 +2,27 @@ package cn.gaoyuexiang.docker.demo.hook.dockerhook.webhook;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.time.ZonedDateTime.now;
+import static java.time.ZoneId.of;
 
 @RestController
 public class HookController {
 
+    private static Logger logger = LoggerFactory.getLogger(HookController.class);
+
     @PostMapping("/webhook/spring-boot-demo")
     public void updateImage() throws IOException {
-        new ProcessBuilder("updateImage.sh").start();
+        Process proc = new ProcessBuilder("updateImage.sh").start();
+        if (proc.exitValue() == 0) {
+            logger.info("Update image successed at {}.", now(of("Asia/Shanghai")).toString());
+        } else {
+            logger.error("Something wrong in updateing image at {}.", now(of("Asia/Shanghai")).toString());
+        }
     }
 
 }

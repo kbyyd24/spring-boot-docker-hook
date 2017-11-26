929bb7b049dede363d61884bf8ad7a6fdafd878e
Yuexiang Gao <yxgao@thoughtworks.com>
2017-11-26 23:06:16 +0800

    map 80 to 8080 instead of host network

--- updateImage.sh
+++ updateImage.sh
@@ -5,4 +5,4 @@ docker stop ${container}
 docker rm ${container}
 docker rmi yuexiang/spring-boot-demo
 docker pull yuexiang/spring-boot-demo
-docker run -d --name=${container} --network=host yuexiang/spring-boot-demo
+docker run -d --name=${container} -p 80:8080 yuexiang/spring-boot-demo
